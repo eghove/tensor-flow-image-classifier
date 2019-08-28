783fd28a28c1b98a25f9ae3a9c008e702fcad499
That_Guy <eghove@gmail.com>
2019-08-27 19:44:37 -0500

    setting model asychs and webcam

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// setting the webcam variable
+const webcamElement = document.getElementById('webcam');
 // setting variable net
 let net;
 
@@ -10,9 +12,44 @@ async function app() {
   console.log('successfully loaded model');
 
   // make a prediction using the model and the image we upload
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result)
+  // const imgEl = document.getElementById('img');
+  // const result = await net.classify(imgEl);
+
+  await setupWebcam();
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+    prediction: ${result[0].className}\n
+    probability: ${result[0].probability}
+    `;
+
+    // breathing room for next animation frame to fire
+
+    await tf.nextFrame();
+
+  }
+  
+}
+
+// webcam function
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({ video: true },
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
 }
 
 // invoke app()
